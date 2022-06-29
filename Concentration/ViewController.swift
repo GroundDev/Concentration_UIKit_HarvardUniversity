e52816edc2d2e873d414ac4a97389952a67e5817
GroundDev <27danny@naver.com>
2022-06-29 18:15:43 +0900

    작업 업데이트

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by KimJS on 2022/06/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    var flipCount: Int = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        let cardNumber = cardButtons.firstIndex(of: sender)
+        print("cardNumber = \(cardNumber)")
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        print("flipCard(withEmoji: \(emoji))")
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = .orange
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = .white
+        }
+    }
+    
+}
+
