bc1f2cb403ea5e1449963b1e288b4ea3c0ad13b8
GroundDev <27danny@naver.com>
2022-07-02 15:56:35 +0900

    editing

---
+++ Concentration.swift
@@ -0,0 +1,20 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by KimJS on 2022/07/01.
+//
+
+import Foundation
+
+class Concentration {
+    var cards: Array<Card>
+    
+    init(cards: Array<Card>) {
+        self.cards = cards
+    }
+    
+    func chooseCard(at index: Int) {
+        
+    }
+}
---
+++ Concentration/Card.swift
@@ -0,0 +1,12 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by KimJS on 2022/07/01.
+//
+
+import Foundation
+
+struct Card {
+    
+}
