6c4d405c5c611231203ce97ca52f3e15f42cf30a
GroundDev <27danny@naver.com>
2022-07-07 02:14:25 +0900

    Video 2 : ~55:06

--- Concentration.swift
+++ Concentration.swift
@@ -8,13 +8,21 @@
 import Foundation
 
 class Concentration {
-    var cards: Array<Card>
-    
-    init(cards: Array<Card>) {
-        self.cards = cards
-    }
+    var cards =  [Card]()
     
     func chooseCard(at index: Int) {
-        
+        if cards[index].isFaceUp {
+            cards[index].isFaceUp = false
+        } else {
+            cards[index].isFaceUp = true
+        }
+    }
+    
+    init(numberOfPairsOfCards: Int) {
+        for _ in 1...numberOfPairsOfCards {
+            let card = Card()
+            cards += [card, card]
+        }
+        // TODO: Shuffle the cards
     }
 }
